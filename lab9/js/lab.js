569b4c17e390fde5cb0c910d48b20e6b72e43117
tyranusaris <elsophbrown@gmail.com>
2025-06-03 20:07:50 -0700

    hello

---
+++ lab9/js/lab.js
@@ -0,0 +1,37 @@
+// lab.js this lab is about arrays and objects
+// Author: Ty Brown and Zeya Hazelman
+// Date: May 8 2025
+/*
+lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+Requirements: jQuery must be loaded for this script to work.
+*/
+
+//append buttons
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+
+$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+
+
+$("#button-challenge").click(function(){
+
+$("#challenge").toggleClass("special");
+
+});
+
+
+$("#button-problems").click(function(){
+  
+$("#problems").toggleClass("special");
+  
+});
+
+
+$("#button-reflection").click(function(){
+
+$("#reflection").toggleClass("special");
+    
+});
